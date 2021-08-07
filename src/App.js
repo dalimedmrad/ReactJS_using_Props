19b933aa4f35cd7fdb131fb7c06578490f24489b
dalimedmrad <dalimohamed789@gmail.com>
2021-08-07 11:09:42 +0100

    PRPS

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import Profile from './profile/Profile';
+
+function App() {
+ 
+  const handleName = (name) =>{
+    alert(`Hi, I am ${name}`) }
+  const sty = {
+    backgroundImage: 'url(b.jpg)',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    backgroundPosition: 'center',
+    backgroundSize: 'auto',
+    margin:'0',
+    padding:'0'
+  }
+
+  return (
+    <div style ={sty}>
+      <Profile 
+          name="mohamed Ali mrad"
+          biog= {[
+            "I am 22 years old",
+            "I am a junior full stack JS developer",
+            'I am from oudref'
+          ]} 
+          profession="I'm a student"
+          handleName = {handleName}>
+          <img width="650"  src="https://www.thebalancecareers.com/thmb/N0WHprMT46rOtd_0GXNO1YYKNww=/1500x1000/filters:fill(auto,1)/web-developer-job-description-salary-and-skills-2061841_final-01-1daf6d8219bd49a089189b4e8c6a7e4d.png" alt="web"/> 
+      </Profile>
+    </div>
+  );
+}
+
+export default App;
